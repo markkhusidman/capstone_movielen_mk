d2dff22eeb6a7ead291e24718f753d88b24cffda
markk <markkhusidman@gmail.com>
2023-03-01 14:34:12 -0500

    Removed all "nrev" features. As a result, the "lambda_b" parameter is obsolete. "lambda_a" has been renamed to just "lambda". Began to clean up code and substitute code duplications with functions.

--- movie-project.r
+++ movie-project.r
@@ -66,12 +66,11 @@ removed <- anti_join(temp, final_holdout_test)
 edx <- rbind(edx, removed)
 rm(dl, ratings, movies, test_index, temp, movielens, removed)
 
+
 print(edx$genres[str_detect(edx$genres, "[A-Z][a-z]+|
 +            [A-Z][a-z]+\\|+[A-Z][a-z]", negate = TRUE)])
 print(edx$title[str_detect(edx$title, "\\s\\(\\d+\\)$", negate = TRUE)])
 
-# temp <- createDataPartition(y = edx$rating, times = 1, p = 0.15, list = FALSE)
-# edx <- edx[temp,]
 
 # Genres 6, 7, and 8 collectively account for only 0.868% of data
 
@@ -102,48 +101,44 @@ final_holdout_test[, movieId := factor(movieId)]
 # Separate genres and calculate number of genres given. Convert genres to 
 # factors
 
-edx[, c("genre_1", "genre_2", "genre_3", "genre_4", "genre_5") := 
-      tstrsplit(edx$genres, "|", fixed=TRUE, fill = "None", keep = 1:5)]
+separate_genres <- function(dt){
+  
+  dt[, c("genre_1", "genre_2", "genre_3", "genre_4", "genre_5") := 
+        tstrsplit(dt$genres, "|", fixed=TRUE, fill = "None", keep = 1:5)]
+  
+  dt[, n_genres := factor(rowSums(.SD != "None")), .SDcols = 
+        c("genre_1", "genre_2", "genre_3", "genre_4", "genre_5")]
+  
+  dt[, c("genre_1", "genre_2", "genre_3", "genre_4", "genre_5") :=
+        list(factor(genre_1), factor(genre_2), factor(genre_3), factor(genre_4),
+             factor(genre_5))]
+}
 
-edx[, n_genres := factor(rowSums(.SD != "None")), .SDcols = 
-      c("genre_1", "genre_2", "genre_3", "genre_4", "genre_5")]
-
-edx[, c("genre_1", "genre_2", "genre_3", "genre_4", "genre_5") :=
-      list(factor(genre_1), factor(genre_2), factor(genre_3), factor(genre_4),
-           factor(genre_5))]
-
-final_holdout_test[, c("genre_1", "genre_2", "genre_3", "genre_4", "genre_5") := 
-      tstrsplit(final_holdout_test$genres, "|", fixed=TRUE, fill = "None", keep = 1:5)]
-
-final_holdout_test[, n_genres := factor(rowSums(.SD != "None")), .SDcols = 
-      c("genre_1", "genre_2", "genre_3", "genre_4", "genre_5")]
-
-final_holdout_test[, c("genre_1", "genre_2", "genre_3", "genre_4", "genre_5") :=
-      list(factor(genre_1), factor(genre_2), factor(genre_3), factor(genre_4),
-           factor(genre_5))]
+separate_genres(edx)
+separate_genres(final_holdout_test)
 
 
 # Extract time-stamp data
-iso <- as_datetime(edx$timestamp)
 
-edx[, c("ts_year", "ts_month", "ts_day", "ts_hour") := 
-      list(factor(year(iso)), factor(month(iso)), 
-           factor(day(iso)), factor(hour(iso)))]
+extract_ts <- function(dt){
+  iso <- as_datetime(dt$timestamp)
+  
+  dt[, c("ts_year", "ts_month", "ts_day", "ts_hour") := 
+        list(factor(year(iso)), factor(month(iso)), 
+             factor(day(iso)), factor(hour(iso)))]
+}
 
-iso <- as_datetime(final_holdout_test$timestamp)
-
-final_holdout_test[, c("ts_year", "ts_month", "ts_day", "ts_hour") := 
-      list(factor(year(iso)), factor(month(iso)), 
-           factor(day(iso)), factor(hour(iso)))]
+extract_ts(edx)
+extract_ts(final_holdout_test)
 
 
 # Extract movie year from title
 
-m_year <- str_match(edx$title, "\\s\\((\\d+)\\)$")[,2]
-edx[, movie_year := factor(year(mdy(paste("1-1-", m_year))))]
+m_year_edx <- str_match(edx$title, "\\s\\((\\d+)\\)$")[,2]
+edx[, movie_year := factor(year(mdy(paste("1-1-", m_year_edx))))]
 
-m_year2 <- str_match(final_holdout_test$title, "\\s\\((\\d+)\\)$")[,2]
-final_holdout_test[, movie_year := factor(year(mdy(paste("1-1-", m_year2))))]
+m_year_fht <- str_match(final_holdout_test$title, "\\s\\((\\d+)\\)$")[,2]
+final_holdout_test[, movie_year := factor(year(mdy(paste("1-1-", m_year_fht))))]
 
 
 # Calculate and remove biases
@@ -152,127 +147,76 @@ global_mean <- mean(edx$rating)
 edx[, unbiased := rating - global_mean]
 
 # elen_pars <- seq(5, 25, 10)
-# gsize_pars <- seq(20, 100, 40)
-# lambda_a_pars <- seq(5, 505, 100)
-# lambda_b_pars <- seq(3, 12, 3)
+# lambda_pars <- seq(5, 505, 100)
 
 elen_pars <- c(25)
-gsize_pars <- c(60)
-lambda_a_pars <- c(5)
-lambda_b_pars <- c(6)
+lambda_pars <- c(5)
 
-params <- expand.grid(elen_pars, gsize_pars, lambda_a_pars, lambda_b_pars)
-names(params) <- c("elen_pars", "gsize_pars", "lambda_a_pars", "lambda_b_pars")
+params <- expand.grid(elen_pars, lambda_pars)
+names(params) <- c("elen_pars", "lambda_pars")
 params$rmse <- NaN
 params$sd <- NaN
 
 
 
 
-# for(row in 1:nrow(params)){
-#   print(params[row,])
-#   print("---------------------------------------------------------------")
-#   rmses <- c()
-#   
-#   # Extract movie era from title
-#   era_len <- params$elen_pars[row]
-#   edx[, movie_era := factor(floor(as.integer(m_year) / era_len) * era_len)]
-#   
-#   # Make sure all variables to be used as join keys are factors or characters
-#   edx[, userId := factor(userId)]
-#   
-#   # Extract user group
-#   group_size = params$gsize_pars[row]
-#   edx[, user_group := factor(ceiling(as.integer(userId) / group_size))]
-#   
-#   # Determine which features should be used to calculate "nrev" biases
-#   # print(apply(edx, 2, function(v){length(unique(v))}))
-#   # str(edx)
-#   
-#   
-#   lambda_a = params$lambda_a_pars[row]
-#   lambda_b = params$lambda_b_pars[row]
-#   
-#   for(i in 1:3){
-#     validate_index <- createDataPartition(y = edx$rating, times = 1, p = 0.1, list = FALSE)
-#     train <- edx[-validate_index,]
-#     validate <- edx[validate_index,]
-#     # temp <- copy(validate)
-#     
-#     # for(sj_col in names(train)){
-#     #   validate <- semi_join(validate, train, by = sj_col)
-#     # }
-#     
-#     validate <- validate |>
-#       semi_join(train, by = "movieId") |> semi_join(train, by = "userId")
-#     
-#     # removed <- anti_join(temp, validate, by = c("userId", "movieId"))
-#     # train <- rbind(train, removed)
-#     
-#     # print(sum(apply(is.na(validate), 1, any)))
-#     # print("Starting bias column joins")
-#     
-#     for(col in c("genre_1", "genre_2", "genre_3", "genre_4", "genre_5", "n_genres",
-#                  "movie_era", "movie_year", "ts_hour", "ts_day", "ts_month", 
-#                  "ts_year")){
-#     
-#       train[, paste0(col, "_bias") := sum(unbiased) / (length(unbiased) + lambda_a),
-#           by = col]
-#     
-#       train[, unbiased := unbiased - .SD, .SDcols = paste0(col, "_bias")]
-#       
-#       # validate <- validate[train[, .SD[1], .SDcols = paste0(col, "_bias"), by= col], on = col, nomatch=NULL]
-#       temp <- train[, .SD[1], .SDcols = paste0(col, "_bias"), by= col]
-#       validate <- temp[validate, on = col]
-#       # validate <- validate[complete.cases(validate)]
-#       # print(col)
-#       # print(sum(apply(is.na(validate), 1, any)))
-#     }
-#     
-#     for(col in c("genres", "movieId", "user_group", "userId")){
-# 
-#       train[, paste0(col, "_nrev") := ceiling(length(unbiased) / 100), by = col]
-# 
-#       train[, paste0(col, "_nrev_bias") :=
-#             sum(unbiased) / (length(unbiased) + lambda_b),
-#           by = eval(paste0(col, "_nrev"))]
-# 
-#       train[, unbiased := unbiased - .SD, .SDcols = paste0(col, "_nrev_bias")]
-# 
-#       train[, paste0(col, "_bias") := sum(unbiased) / (length(unbiased) + lambda_b),
-#           by = col]
-# 
-#       train[, unbiased := unbiased - .SD, .SDcols = paste0(col, "_bias")]
-#       train[, paste0(col, "_nrev") := NULL]
-#       
-#       # validate <- validate[train[, .SD[1], .SDcols = paste0(col, c("_bias", "_nrev_bias")), by= col], on = col, nomatch=NULL]
-#       temp <- train[, .SD[1], .SDcols = paste0(col, c("_bias", "_nrev_bias")), by = col]
-#       validate <- temp[validate, on = col]
-#       # validate <- validate[complete.cases(validate)]
-#       # print(col)
-#       # print(sum(apply(is.na(validate), 1, any)))
-#     }
-#   
-#     # print(names(validate))
-#     col_names <- names(train)
-#     bias_cols <- col_names[str_detect(col_names, "_bias")]
-#     validate[, pred := rowSums(.SD) + global_mean, .SDcols = bias_cols]
-#     
-#     # Clip predictions such that they are between 0 and 5
-#     validate[, pred := fifelse(pred > 5, 5, pred)]
-#     validate[, pred := fifelse(pred < 0, 0, pred)]
-#     
-#     rmses[i] <- sqrt(mean((validate$pred - validate$rating)^2))
-#     print(rmses[i])
-#   }
-#   
-#   params$rmse[row] <- mean(rmses)
-#   params$sd[row] <- sd(rmses)
-# }
-# 
-# print(params[order(params$rmse),])
-# # fwrite(params, "temp_model-tuning.csv")
+for(row in 1:nrow(params)){
+  print(params[row,])
+  print("---------------------------------------------------------------")
+  rmses <- c()
 
+  # Extract movie era from title
+  era_len <- params$elen_pars[row]
+  edx[, movie_era := factor(floor(as.integer(m_year_edx) / era_len) * era_len)]
+
+  # Make sure all variables to be used as join keys are factors or characters
+  edx[, userId := factor(userId)]
+
+  lambda = params$lambda_pars[row]
+
+  for(i in 1:3){
+    validate_index <- createDataPartition(y = edx$rating, times = 1, p = 0.1, list = FALSE)
+    train <- edx[-validate_index,]
+    temp <- edx[validate_index,]
+
+    validate <- temp |>
+      semi_join(train, by = "movieId") |> semi_join(train, by = "userId")
+
+    removed <- anti_join(temp, validate, by = c("userId", "movieId"))
+    train <- rbind(train, removed)
+
+    for(col in c("genre_1", "genre_2", "genre_3", "genre_4", "genre_5", "n_genres",
+                 "movie_era", "movie_year", "ts_hour", "ts_day", "ts_month",
+                 "ts_year", "movieId", "userId")){
+
+      train[, paste0(col, "_bias") := sum(unbiased) / (length(unbiased) + lambda),
+          by = col]
+
+      train[, unbiased := unbiased - .SD, .SDcols = paste0(col, "_bias")]
+
+      temp <- train[, .SD[1], .SDcols = paste0(col, "_bias"), by= col]
+      validate <- temp[validate, on = col]
+    }
+
+    # print(names(validate))
+    col_names <- names(train)
+    bias_cols <- col_names[str_detect(col_names, "_bias")]
+    validate[, pred := rowSums(.SD) + global_mean, .SDcols = bias_cols]
+
+    # Clip predictions such that they are between 0 and 5
+    validate[, pred := fifelse(pred > 5, 5, pred)]
+    validate[, pred := fifelse(pred < 0, 0, pred)]
+
+    rmses[i] <- sqrt(mean((validate$pred - validate$rating)^2))
+    print(rmses[i])
+  }
+
+  params$rmse[row] <- mean(rmses)
+  params$sd[row] <- sd(rmses)
+}
+
+print(params[order(params$rmse),])
+# fwrite(params, "temp_model-tuning.csv")
 
 
 
@@ -314,31 +258,21 @@ params$sd <- NaN
 
 # Extract movie era from title
 era_len <- 25
-edx[, movie_era := factor(floor(as.integer(m_year) / era_len) * era_len)]
-final_holdout_test[, movie_era := factor(floor(as.integer(m_year2) / era_len) * era_len)]
+edx[, movie_era := factor(floor(as.integer(m_year_edx) / era_len) * era_len)]
+final_holdout_test[, movie_era := factor(floor(as.integer(m_year_fht) / era_len) * era_len)]
 
 # Make sure all variables to be used as join keys are factors or characters
 edx[, userId := factor(userId)]
 final_holdout_test[, userId := factor(userId)]
 
-# Extract user group
-group_size = 60
-edx[, user_group := factor(ceiling(as.integer(userId) / group_size))]
-final_holdout_test[, user_group := factor(ceiling(as.integer(userId) / group_size))]
-
-# Determine which features should be used to calculate "nrev" biases
-# print(apply(edx, 2, function(v){length(unique(v))}))
-# str(edx)
-
-lambda_a = 5
-lambda_b = 6
+lambda = 5
 
 for(col in c("genre_1", "genre_2", "genre_3", "genre_4", "genre_5", "n_genres",
              "movie_era", "movie_year", "ts_hour", "ts_day", "ts_month", 
-             "ts_year")){
+             "ts_year", "movieId", "userId")){
   
-  edx[, paste0(col, "_bias") := sum(unbiased) / (length(unbiased) + lambda_a),
-        by = col]
+  edx[, paste0(col, "_bias") := sum(unbiased) / (length(unbiased) + lambda),
+      by = col]
   
   edx[, unbiased := unbiased - .SD, .SDcols = paste0(col, "_bias")]
   
@@ -347,29 +281,6 @@ for(col in c("genre_1", "genre_2", "genre_3", "genre_4", "genre_5", "n_genres",
  
 }
 
-for(col in c("movieId", "userId")){
-  
-  edx[, paste0(col, "_nrev") := ceiling(length(unbiased) / 100), by = col]
-  
-  edx[, paste0(col, "_nrev_bias") :=
-          sum(unbiased) / (length(unbiased) + lambda_b),
-        by = eval(paste0(col, "_nrev"))]
-  
-  edx[, unbiased := unbiased - .SD, .SDcols = paste0(col, "_nrev_bias")]
-  
-  edx[, paste0(col, "_bias") := sum(unbiased) / (length(unbiased) + lambda_b),
-        by = col]
-  
-  edx[, unbiased := unbiased - .SD, .SDcols = paste0(col, "_bias")]
-  edx[, paste0(col, "_nrev") := NULL]
-  
-  # validate <- validate[train[, .SD[1], .SDcols = paste0(col, c("_bias", "_nrev_bias")), by= col], on = col, nomatch=NULL]
-  temp <- edx[, .SD[1], .SDcols = paste0(col, c("_bias", "_nrev_bias")), by = col]
-  final_holdout_test <- temp[final_holdout_test, on = col]
-  # validate <- validate[complete.cases(validate)]
-  # print(col)
-  # print(sum(apply(is.na(validate), 1, any)))
-}
 
 col_names <- names(edx)
 bias_cols <- col_names[str_detect(col_names, "_bias")]
